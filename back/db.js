7387b1e5bcd87fc327c5482d668ef1e477508e09
pce0303 <ivychoen@gmail.com>
2023-12-21 22:59:39 +0900

    database connection

--- back/db.js
+++ back/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: '',
-    password: '',
+    user: 'wisher',
+    password: 'blue',
     database: '2024preview'
 });
 
